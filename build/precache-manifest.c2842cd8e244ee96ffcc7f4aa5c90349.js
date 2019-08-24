9a3912afeff1efe2831e4e58b02245223a0a52e2
Connor Scoma <connorscoma@live.com>
2019-08-24 13:50:55 -0500

    adding production build

---
+++ build/precache-manifest.c2842cd8e244ee96ffcc7f4aa5c90349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7fd327b54c38fe89cd14308413b3ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba120a5af0985e1d7756",
+    "url": "/static/css/main.401a8d98.chunk.css"
+  },
+  {
+    "revision": "d2114e31dd526cf7b898",
+    "url": "/static/js/2.8ad76ec9.chunk.js"
+  },
+  {
+    "revision": "ba120a5af0985e1d7756",
+    "url": "/static/js/main.43878b7a.chunk.js"
+  },
+  {
+    "revision": "cf2e5e8d5d54131d754a",
+    "url": "/static/js/runtime~main.1c9f0724.js"
+  }
+]);
